94f0adff131d94c7822331817013209cfeeb88ae
Symon-Maraga <32030986+Symon-Maraga@users.noreply.github.com>
2017-09-18 07:08:15 +0300

    Add files via upload

---
+++ po.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+name = input('Sema jina lako bwana!: ')
+rawAge = input('Uko miaka ngapi msee!:  ')
+age = int(rawAge)
+
+if age > 20:
+  print(name, 'Hapo sawa msee!, Tunaeza bonga sasa, Lakini ingia Kwanza')
+else:
+  print(name, 'hizo miaka hazijatosha!!' 'ongeza miaka kwanza,' 'Nugu ', 'kitunguu saumu, .')
